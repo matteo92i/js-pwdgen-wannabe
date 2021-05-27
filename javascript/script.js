9d987eb721f26a6ec5008e596b53515267689ab6
Matteo92i <matteo.iovane@gmail.com>
2021-05-27 22:29:22 +0200

    fine psw

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+var nomeUtente = prompt('inserisci nome');
+var cognomeUtente = prompt ('inserisci cognome');
+var colorePreferito = prompt('inserisci colore preferito');
+var numero = 21
+document.getElementById('password').innerHTML =  nomeUtente + cognomeUtente + colorePreferito + numero;
