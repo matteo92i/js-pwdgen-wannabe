d12ab26d8166c597ea80ed5b2f6559701545668f
Matteo92i <matteo.iovane@gmail.com>
2021-08-04 15:34:08 +0200

    modifiche

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,9 @@
-var nomeUtente = prompt('inserisci nome');
-var cognomeUtente = prompt ('inserisci cognome');
-var colorePreferito = prompt('inserisci colore preferito');
+var enterName = prompt('inserisci nome');
+var enterSurname = prompt ('inserisci cognome');
+var favoriteColor = prompt('inserisci colore preferito');
 var numero = 21
-document.getElementById('password').innerHTML =  nomeUtente + cognomeUtente + colorePreferito + numero;
+document.getElementById('password').innerHTML =  enterName + enterSurname + favoriteColor + numero;
+
+document.getElementById('enterName').innerHTML = "nome:" + enterName;
+document.getElementById('enterSurname').innerHTML = "cognome:" + enterSurname;
+document.getElementById('favoriteColor').innerHTML = "colore preferito:" + favoriteColor;
